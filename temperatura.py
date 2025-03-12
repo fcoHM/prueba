e9a0472eebde51f2908439e3825a40c666325084
Francisco <josefrancisco45rh@outlook.com>
2025-03-11 21:55:52 -0600

    para ejemplo de conflictos

--- temperatura.py
+++ temperatura.py
@@ -2,6 +2,6 @@ print("medir temperatura")
 
 print("ya tenemos la configuracion del sensor")
 
-print("ya esta terminado el codigo")
+print("ya esta terminado el codigo, no seas metiroso, aun falta")
 
 print("mi primer commit")
